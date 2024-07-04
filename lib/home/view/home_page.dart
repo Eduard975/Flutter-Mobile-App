dce7b1fa23f387a904c167c768aec19d0b83c9fa
Eduard975 <theblackasasin@yahoo.com>
2024-07-04 11:47:07 +0300

    edit home page UI

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -9,29 +9,46 @@ class HomePage extends StatelessWidget {
     return MaterialPageRoute<void>(builder: (_) => const HomePage());
   }
 
+  double deviceHeight(BuildContext context) =>
+      MediaQuery.of(context).size.height;
+
+  double deviceWidth(BuildContext context) => MediaQuery.of(context).size.width;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.only(
+          top: deviceHeight(context) * 0.05,
+          right: deviceWidth(context) * 0.05,
+        ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Builder(
-              builder: (context) {
-                final userId = context.select(
-                  (AuthenticationBloc bloc) => bloc.state.user.id,
-                );
-                return Text('UserID: $userId');
-              },
-            ),
-            ElevatedButton(
-              child: const Text('Logout'),
-              onPressed: () {
-                context
-                    .read<AuthenticationBloc>()
-                    .add(AuthenticationLogoutRequested());
-              },
-            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Row(mainAxisSize: MainAxisSize.min, children: [
+                Builder(
+                  builder: (context) {
+                    final userId = context.select(
+                      (AuthenticationBloc bloc) => bloc.state.user.name,
+                    );
+                    return Text('Welcome $userId!');
+                  },
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(12),
+                ),
+                ElevatedButton(
+                  child: const Text('Logout'),
+                  onPressed: () {
+                    context
+                        .read<AuthenticationBloc>()
+                        .add(AuthenticationLogoutRequested());
+                  },
+                ),
+              ]),
+            )
           ],
         ),
       ),
